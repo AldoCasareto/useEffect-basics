053b1821e937ee6eebe45d214703dd0ce2937d10
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2021-03-09 16:59:41 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import MyPractice from './tutorial/1-useState/setup/My-practice';
+// import ErrorExample from './tutorial/1-useState/setup/1-error-example'
+// import ErrorExample from './tutorial/1-useState/setup/2-useState-basics';
+// import ErrorExample from './tutorial/1-useState/setup/3-useState-array';
+// import ErrorExample from './tutorial/1-useState/setup/5-useState-counter';
+// import ErrorExample from './tutorial/2-useEffect/setup/1-useEffect-basics';
+// import ErrorExample from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import ErrorExample from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+
+function App() {
+  return (
+    <div className='container'>
+      {/* <ErrorExample /> */}
+      <ErrorExample />
+    </div>
+  );
+}
+
+export default App;
